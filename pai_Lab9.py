67c289053072834b2563a3eb9b73aeda6a4abe19
sylvan-blake <sylvan.blake@ufl.edu>
2024-07-24 14:38:04 -0400

    Finished Dedcode function

--- pai_Lab9.py
+++ pai_Lab9.py
@@ -14,7 +14,6 @@ def decode(password):
         opassword = opassword + str(i)
     print(f"The encoded password is {password}, and the original password is {opassword}")
 
-
 def menu(): # prints menu
     print("Menu")
     print("-------------")
